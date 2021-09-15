8c34fe24e794091debc02ac4e061dad6d3606a8b
Huyskiz <huytran0444@gmail.com>
2021-09-15 16:54:45 +1000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -22,29 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <div><h2>PHP React App</h2>
-      <form>
-        <div className ="form-input">
-          <input
-          type = "text"
-          name = "title"
-          placeholder = "Title"
-          value = {this.state.title}
-          onChange = {this.handleChange}
-          />
-        </div>
-        <div className = "form-input">
-          <textarea
-          placeholder = "body"
-          name = "body"
-          cols = "30"
-          rows = "10"
-          value = {this.state.body}
-          onChange = {this.handleChange}
-          >
-          </textarea>
-        </div>
-        <button> Submit</button>
-      </form>
+     
       </div>
     );
   }
