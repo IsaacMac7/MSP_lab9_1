9d0c2363fe8133b4849324237df7050fc2766d4c
IsaacMac <isaacmacandog7@gmail.com>
2021-10-14 16:26:03 +1100

    Fetched Api but Graph incomplete

--- client/src/Graph.js
+++ client/src/Graph.js
@@ -1,20 +1,121 @@
-import React from "react";
-import {Line} from 'react-chartjs-2'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {Bar, Line} from 'react-chartjs-2'
 import './App.css';
 
 
-function LineChart () {
-  const data = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
-    datasets: [
-      {
-        label: 'Sales for 2020',
-        data: [3, 2, 2, 2, 1]
-      }
-    ]  
+function Graph () {
+  const [chartData, setChartData] = useState({});
+  var [salesList, setSalesList] = useState([]);
+  const [salesId, setSalesId] = useState(0);
+  const [stockInfo, setStockInfo] = useState("");
+  const [stockDate, setstockDate] = useState("");
+  const [stockAmt, setstockAmt] = useState(0);
+  const [salesPrice, setsalesPrice] = useState(0);
+  
+
+  // //get API
+  // useEffect(()=>{
+  //   axios.get('http://localhost:8080/salesapi/read').then((response)=>{
+  //     setSalesList(response.data);
+  //   })
+
+  // }, [salesList]) 
+
+  // //sort for panadol
+  // var result = salesList.filter(x => x.stockInfo === "Stock ID: 6 Stock Name: Panadol $15");
+  // console.log(result);
+
+  // //canvas for graph
+  // return (
+  //   <div>
+  //     <Line
+  //       data={{
+  //         labels: [stockDate],
+  //         datasets: [
+  //           {
+  //             label: 'Panadol Sales',
+  //             data: [stockAmt],l
+  //           }
+  //         ]
+  //       }}
+        
+  //     />
+  //   </div>
+  // ) 
+
+  const chart = () => {
+    let empDate = [];
+    let empAmt = [];
+    axios
+      .get('http://localhost:8080/salesapi/read')
+      .then(response => {
+        console.log(response);
+        for (const dataObj of response.data){
+          empDate.push(dataObj.stockDate);
+          empAmt.push(dataObj.stockAmt);
+        }
+        setChartData({
+          labels: empAmt,
+          datasets: [
+            {
+              label: "Date",
+              data: empDate,
+              backgroundColor: ["rgba(75, 192, 192, 0.6)"],
+              borderWidth: 4
+            }
+          ]
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+      console.log(empDate, empDate);
   }
-  return <Line data={data} />
+
+
+
+  useEffect(() => {
+    chart();
+  }, []);
+  return (
+    <div className="Graph">
+      <h1>Graph</h1>
+      <div>
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            title: { text: "THICCNESS SCALE", display: true },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 10,
+                    beginAtZero: true
+                  },
+                  gridLines: {
+                    display: false
+                  }
+                }
+              ],
+              xAxes: [
+                {
+                  gridLines: {
+                    display: false
+                  }
+                }
+              ]
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
+
+  
 }
 
 
-export default LineChart;
+export default Graph;
