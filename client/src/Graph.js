9cfbb4a964844da17597a239df4b404e2c47f5cc
IsaacMac <isaacmacandog7@gmail.com>
2021-10-15 19:35:32 +1100

    made the code more modular
    (still need to update graph)

--- client/src/Graph.js
+++ client/src/Graph.js
@@ -1,104 +1,85 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {Bar, Line} from 'react-chartjs-2'
+import {Bar, Line} from 'react-chartjs-2';
+import {Chart} from 'react-chartjs-2';
+
 import './App.css';
 
-
-function Graph () {
-  const [chartData, setChartData] = useState({});
-  var [salesList, setSalesList] = useState([]);
-  const [salesId, setSalesId] = useState(0);
-  const [stockInfo, setStockInfo] = useState("");
-  const [stockDate, setstockDate] = useState("");
-  const [stockAmt, setstockAmt] = useState(0);
-  const [salesPrice, setsalesPrice] = useState(0);
-
-  function chart() {
-    let empDate = [];
-    let empAmt = [];
-    axios
-      .get('http://localhost:8080/salesapi/read')
-      .then(response => {
-        setSalesList(response.data);
-        // console.log(response);
-        // console.log(splitID(response));
-        for (const dataObj of response.data) {
-          empDate.push(dataObj.stockDate);
-          empAmt.push(dataObj.stockAmt);
-        }
-        setChartData({
-          labels: empDate,
-          datasets: [
-            {
-              label: "Amount Sold",
-              data: empAmt,
-              backgroundColor: ["rgba(75, 192, 192, 0.6)"],
-              borderWidth: 2
-            }
-          ]
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    // console.log(empDate, empDate);
-  }
-
-  useEffect(() => {
-    chart();
-  }, [salesList]);
-  // console.log("sales array", salesList); 
-  // console.log(splitID(salesList.stockInfo));
-  return (
-    <div className="Graph" class="chart-container">
-      <h1>Graph</h1>
-        <Line
-          data={chartData}
-          options={{
-            maintainAspectRatio: false, //essential for resizing chart
-            responsive: true,
-            title: { text: "SALES", display: true },
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    autoSkip: true,
-                    maxTicksLimit: 10,
-                    beginAtZero: true
-                  },
-                  gridLines: {
-                    display: false
-                  }
-                }
-              ],
-              xAxes: [
-                {
-                  gridLines: {
-                    display: false
-                  }
-                }
-              ]
-            }
-          }}
-        />
-        <button onClick={(onChange) => applyFilter()} value = "salesList" > Click me </button>
-    </div>
-  );
-  
-  function applyFilter(saleList){
-    //check array is being passed through
-    console.log("apply filter (original array):", salesList);
-    //check filtered data works
-    const filteredData = salesList.filter(function(ele){
-      return ele.stockInfo == "Stock ID: 6 Stock Name: Panadol $15";
-    });
-    console.log("apply filter (new array):", filteredData);
-
-  }
+function byID(salesList){
+    const filteredData = salesList.filter(function(item){
+        return item.stockInfo  == "Stock ID: 6 Stock Name: Panadol $15";
+    })
+    console.log("filtered", filteredData)
 }
 
+export default function Graph(){
+    const[salesList, setsalesList] = useState([]);
+    const[dateList, setdateList] = useState([]);
+    const[amtList, setamtList] = useState([]);
+    const[stockList, setStockList] = useState([]);
 
+    //fetch sales data
+    useEffect(()=>{
+        let empDate = [];
+        let empAmt = [];
+        axios.get('http://localhost:8080/salesapi/read').then((response)=>{
+          setsalesList(response.data);
+          for (const dataObj of response.data) {
+              empDate.push(dataObj.stockDate); //dataObj sees it as individual key-value pairs, must be put into an array first (in order)
+              empAmt.push(dataObj.stockAmt);
+          }
+        //   console.log("empDate", empDate);
+        //   console.log("empAmt", empAmt);
+          setdateList(empDate); //once dataObj is in an array, put it in a list
+          setamtList(empAmt);
+        })
+    },[salesList], [dateList], [amtList])
+    // console.log("sales api", salesList);
+    // console.log("stock date", dateList);
+    // console.log("amtList", amtList);
 
+    //fetch stock data
+    useEffect(()=>{
+        axios.get('http://localhost:8080/api/read').then((response)=>{
+          setStockList(response.data);
+        })
+      },[stockList])
+    //   console.log("stock list", stockList);
 
+    //call function that seperates by stockInfo
+    byID(salesList);
+    
+    const chart = {
+        labels: dateList,
+        datasets: [
+            {
+            label: 'Amount of Stock Sold',
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 2,
+            data: amtList
+            }
+        ]
+    };
 
-export default Graph;
+    return (
+        <div >
+        <Line id={"myChart"}
+            data={chart}
+            options={{
+                maintainAspectRatio: false,
+                title:{
+                    display:true,
+                    fontSize:20
+            },
+            legend:{
+                display:true,
+                position:'right'
+            }
+            }}
+        />
+        </div>
+    );
+}
