651c813ad8989b1ef3907c6354210c8164770381
LyanAzidie <30411767+LyanAzidie@users.noreply.github.com>
2021-10-20 10:44:47 +1100

    tried some testing stuff

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {jest} from '@jest/globals';
+import defaultExport from './ExtractSales';
+import axios from "axios";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./ExtractSales');
+console.log(defaultExport);
+/*
+test('Extract user from one date', () => {
+  expect(ExtractSales.App('2021-02-01')).toStrictEqual(new Object());
 });
+*/
